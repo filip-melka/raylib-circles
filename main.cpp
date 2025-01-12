6e93b09792d6074dfe4e9f66a406d72b2da33a63
Filip Melka <melka.filip@proton.me>
2025-01-12 20:35:07 +0100

    feat: add boundaries

--- main.cpp
+++ main.cpp
@@ -2,15 +2,27 @@
 #include "stdlib.h"
 #include <cmath>
 #include <cstdlib>
-#include "vector2.h"
+#include <vector>
+#include "circle.h"
 
 using namespace std;
 
 const int screenWidth = 700;
-const int screenHeight = 500;
+const int screenHeight = 700;
+
+vector2 getRandomNormalizedVector(){
+    float x = (rand() % 2000 - 1000) / 1000.0f;
+    float y = (rand() % 2000 - 1000) / 1000.0f;
+
+    vector2 v {x,y};
+
+    return v.normalize();
+}
 
 int main(void)
 {
+    Circle* boundaries = new Circle(vector2 {screenWidth/2,screenHeight/2}, {0,0}, screenWidth/2.01, BLACK);
+
     InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
 
     SetTargetFPS(60);
@@ -18,9 +30,9 @@ int main(void)
     while (!WindowShouldClose())
     {
         BeginDrawing();
-        ClearBackground(BLACK);
+        ClearBackground(WHITE);
 
-        // DRAW
+        boundaries->Draw();
 
         EndDrawing();
     }
